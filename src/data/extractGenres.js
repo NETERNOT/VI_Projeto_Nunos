15ad895730ac2dc4d4fcf26ed6e1957c06eaffec
NunoPinto <167584289+NETERNOT@users.noreply.github.com>
2025-12-07 18:20:27 +0000

    Update Extract Genre
    Now also saves fans by year.

--- src/data/extractGenres.js
+++ src/data/extractGenres.js
@@ -18,9 +18,11 @@ export function extractGenres(data) {
         let entry = list.find((e) => e.id === style);
         entry.fans.total += band.fans;
 
-        let countryEntry = entry.fans.countries.find((e)=> e.country === band.origin)
+        let countryEntry = entry.fans.countries.find(
+          (e) => e.country === band.origin
+        );
 
-        if(countryEntry){
+        if (countryEntry) {
           countryEntry.fans += band.fans;
         } else {
           entry.fans.countries.push({
@@ -28,18 +30,50 @@ export function extractGenres(data) {
             fans: band.fans,
           });
         }
-        
       }
     });
     return list;
   }, []);
 
-  const withRadius = genreNodeData.map((genre) => {
-    return {
+  const withRadius = genreNodeData.map((genre) => ({
       ...genre,
       radius: Math.max(12.5, Math.sqrt(genre.fans.total * 0.2)),
+  }));
+
+  let dataCopy = data.map((band) => ({
+    ...band,
+    split: band.split === "-" ? "2017" : band.split,
+  }));
+
+  const minDate = Math.min(...dataCopy.map((band) => parseInt(band.formed)));
+  const maxDate = Math.max(...dataCopy.map((band) => parseInt(band.split)));
+
+  const withFansByYear = withRadius.map((genre) => {
+    let fansByYear = [];
+    for (let year = minDate; year < maxDate; year++) {
+      let bandsInYear = dataCopy.filter(
+        (band) => band.origin <= year && year <= band.split
+      );
+
+      bandsInYear.filter((band)=>{
+        return band.style.includes(genre.id)
+      })
+
+      let fansInYear = bandsInYear.reduce((sum, band) => {
+        return sum + band.fans;
+      }, 0);
+
+      fansByYear.push({
+        year,
+        fans: fansInYear,
+      });
+    }
+
+    return {
+      ...genre,
+      fansByYear,
     };
   });
-
-  return withRadius;
+  
+  return withFansByYear;
 }
